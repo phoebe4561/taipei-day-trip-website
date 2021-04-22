c2c2728b75c6ad6819b2787cb20ea4b50200b154
Phoebe Pao <phoebe4561@gmail.com>
2021-04-22 10:41:48 +0800

    insert data into attraction_tb

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask,render_template,request,jsonify
+from data import data
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app=Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:zaq11403@localhost:3306/taipeiprojectdb'
+
+db = SQLAlchemy(app)
+
 # Pages
+@app.route("/load")
+def load():
+	data.load_data(db)
+	return "ok"
+
 @app.route("/")
 def index():
 	return render_template("index.html")
