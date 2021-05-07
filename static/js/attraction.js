ad7caf03c6885a82ff92a0ac70fcf1ef26440044
Phoebe Pao <phoebe4561@gmail.com>
2021-05-07 18:22:08 +0800

    create spotimg slideshow

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,4 +1,5 @@
 let id = location.href.split("/")[4];
+let index = 1;
 async function loadAttractionId() {
   let response = await fetch("/api/attraction/" + id);
   let result = await response.json();
@@ -7,6 +8,28 @@ async function loadAttractionId() {
 
   getImages(imgArr);
   getData();
+  showImage();
+
+  let leftBtn = document.querySelector(".leftBtn");
+  leftBtn.addEventListener("click", () => {
+    changeImage(-1);
+  });
+
+  let rightBtn = document.querySelector(".rightBtn");
+  rightBtn.addEventListener("click", () => {
+    changeImage(1);
+  });
+
+  let dot = document.querySelectorAll(".dot");
+  // console.log(dot);
+  for (i = 0; i < dot.length; i++) {
+    dot[i].addEventListener("click", (e) => {
+      let index = e.target.getAttribute("data-attr");
+      index = parseInt(index);
+      changeDot(index);
+      // console.log(index);
+    });
+  }
 }
 loadAttractionId();
 
@@ -39,4 +62,31 @@ function getData() {
   spotDesc.textContent = data.description;
   spotAdd.textContent = data.address;
   spotTrans.textContent = data.transport;
+}
+
+function changeDot(n) {
+  showImage((index = n));
+}
+
+function changeImage(n) {
+  showImage((index += n));
+}
+
+function showImage(n) {
+  let slides = document.querySelectorAll(".slides");
+  let dot = document.querySelectorAll(".dot");
+  if (n > slides.length) {
+    index = 1;
+  }
+  if (n < 1) {
+    index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].style.background = "black";
+  }
+  slides[index - 1].style.display = "flex";
+  dot[index - 1].style.background = "white";
 }
